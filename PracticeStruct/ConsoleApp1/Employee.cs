ebe6e864b143ab61f9ed1c7f64147af4c188284e
AlisherB <alisher_b_95@mail.ru>
2017-09-07 17:41:40 +0600

    Struct and Enum
    (23.08.17-02.09.17)

---
+++ PracticeStruct/ConsoleApp1/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    struct Employee
+    {
+        private string name;
+        private Vacancies vacancy;
+        private int salary;
+        private int[] date;
+    }
+}
